331f491431eb5673875d01127ce754bec9e56ded
jjokah <johnjohnsonokah@gmai.com>
2022-10-31 04:41:01 +0100

    Set REST_FRAMEWORK permissions settings to AllowAny

--- django_project/settings.py
+++ django_project/settings.py
@@ -132,3 +132,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
